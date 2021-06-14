4a8e8633ed2cb8b53290ac067aa73b508f2e6d1d
EliezerAVS <eliezer.augusto@hotmail.com>
2021-06-14 08:49:51 -0300

    produto

---
+++ app/Models/ProdutoModel.php
@@ -0,0 +1,17 @@
+<?php namespace App\Models;
+  
+use CodeIgniter\Model;
+  
+class ProdutoModel extends Model
+{
+    protected $table = 'produto';
+    protected $primaryKey = 'produto_id';
+    protected $allowedFields = ['nome','descricao','preco','quantidade','codigo_de_barras'];
+    protected $validationRules    = [
+        'nome'              => 'required|max_length[60]',
+        'descricao'         => 'max_length[150]',
+        'preco'             => 'required|max_length[50]',
+        'quantidade'        => 'required|integer',
+        'codigo_de_barras'  => 'max_length[60]'
+    ];
+}
